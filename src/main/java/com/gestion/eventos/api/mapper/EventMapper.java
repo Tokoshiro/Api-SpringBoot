a0476c90b512069c83cbe79b0e97f3edd6e64833
Tokoshiro <dgarmendia69@gmail.com>
2026-04-14 17:23:07 -0500

    Mapper: Category, Speaker Role y Event

--- src/main/java/com/gestion/eventos/api/mapper/EventMapper.java
+++ src/main/java/com/gestion/eventos/api/mapper/EventMapper.java
@@ -3,17 +3,34 @@ package com.gestion.eventos.api.mapper;
 import com.gestion.eventos.api.domain.Event;
 import com.gestion.eventos.api.dto.EventRequestDto;
 import com.gestion.eventos.api.dto.EventResponseDto;
+import com.gestion.eventos.api.dto.EventSummaryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
-    Event toEntity(EventRequestDto eventRequestDto);
-    EventResponseDto toResponseDto(Event event);
 
+    //Mapeo de entrada - Request Dto
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "speaker", ignore = true)
+    @Mapping(target = "attendedUser", ignore = true)
+    Event toEntity(EventRequestDto eventRequestDto);
+
+    //Mapeo de salida - Request Dto
+    EventResponseDto toResponseDto(Event event);
     List<EventResponseDto> toEventResponseDtoList(List<Event> events);
 
+    //Metodo para actualizar una Entidad Existente
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "speaker", ignore = true)
+    @Mapping(target = "attendedUser", ignore = true)
     void updateEventFromDto(EventRequestDto dto, @MappingTarget Event event);
+
+    EventSummaryDto toSummaryDto(Event event);
+    List<EventSummaryDto> tSummaryDtoList(List<Event> events);
+
 }
---
+++ src/main/java/com/gestion/eventos/api/mapper/SpeakerMapper.java
@@ -0,0 +1,14 @@
+package com.gestion.eventos.api.mapper;
+
+
+import com.gestion.eventos.api.domain.Speaker;
+import com.gestion.eventos.api.dto.SpeakerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SpeakerMapper {
+
+    SpeakerDto toDto(Speaker speaker);
+    Speaker toEntity(Speaker speakerDto);
+
+}
