229322cf5629d527be6a3b579cb39e1d95b6a124
Tokoshiro <dgarmendia69@gmail.com>
2026-04-30 22:55:24 -0500

    solución Join Fetch

--- src/main/java/com/gestion/eventos/api/repository/EventRepository.java
+++ src/main/java/com/gestion/eventos/api/repository/EventRepository.java
@@ -4,12 +4,20 @@ import com.gestion.eventos.api.domain.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     Page<Event> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
+    @Query("Select e from Event e Join Fetch e.category Left Join Fetch e.speakers")
+    List<Event> findAllWithCategoryAndSpeaker();
 
-
+    @Query("Select e from Event e Join Fetch e.category Left Join Fetch e.speakers Where e.id = :id")
+    Optional<Event> findByIdWithCategoryAndSpeaker(Long id);
 }
